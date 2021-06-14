850304c527307f0d205efc2e00be504241cc9d3a
venusdharan <venusdharan@gmail.com>
2021-06-14 07:42:23 +0530

    initial

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Hapi = require('@hapi/hapi');
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
+        port: process.env.PORT || 3000,
         host: 'localhost'
     });
 
